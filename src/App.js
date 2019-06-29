a2dd306902715e2f644c04508fec9559a6ba81d1
Caleb Druckemiller <cdruckemiller@versatilets.com>
2019-06-29 17:11:48 -0400

    Init commit of football picker app
    Add dependencies required for following tools:
    * prettier
    * eslint
    * parcel
    * React
    
    Also npm installed react-router-dom

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react" 
+import { render } from "react-dom"
+import { HashRouters, Route, Switch} from "react-router-dom"
+
+const App = () => {
+        
+        return (
+                <div>
+                    <h1>New React App</h1>
+                </div>
+            
+                 
+         )
+ }       
+         
+render(<App />, document.getElementById("root"))
