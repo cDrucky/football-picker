b382807b186401481e8c1637a4b9abed3830e104
Caleb Druckemiller <cdruckemiller@versatilets.com>
2019-06-29 17:50:59 -0400

    Add Route to PickerView

---
+++ src/components/domain/picker/view/PickerView.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react"
+
+
+class PickerView extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {  }
+    }
+    render() { 
+        return (  
+            <h1>My Picker View</h1>
+        )
+    }
+}
+ 
+export default PickerView
